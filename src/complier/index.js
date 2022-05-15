99a555913242716c5a6df9252dda31347ba0150e
GuYun-D <1946306647@qq.com>
2022-05-15 12:08:15 +0800

    (> compiler:[index.js], init.js)模板编译前的初始化

---
+++ src/complier/index.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {*} template 
+ */
+export function complieToFunction(template){
+  console.log("编译处理的模板", template);
+
+  // 将模板生成ast语法树
+
+  // 生成render方法，render方法执行的结果就是 虚拟dom
+}
